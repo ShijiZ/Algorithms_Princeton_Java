782bf32f4c1d070f4a0295212b2216ab2a789fd6
Shiji Zhao <zhaoshiji2016@gmail.com>
2019-08-06 22:47:52 -0700

    Update section 4.2

---
+++ src/Digraph.java
@@ -0,0 +1,71 @@
+/*
+ * Sample usage: java-alg4 Graph tinyDG.txt
+ * Sample usage: java-alg4 Graph mediumDG.txt
+ */
+
+public class Digraph {
+    private final int V;               // number of vertices
+    private int E;                     // number of edges
+    private LinkedBag<Integer>[] adj;  // adjacency lists
+
+    public Digraph(int V){
+        this.V = V;
+        this.E = 0;
+        adj = (LinkedBag<Integer>[]) new LinkedBag[V];  // Create array of lists
+        for (int v = 0; v < V; v++)            // Initialize all lists to empty
+            adj[v] = new LinkedBag<Integer>();
+    }
+
+    public Digraph(In in){
+        this(in.readInt());       // read V and construct this graph
+        int E = in.readInt();     // read E
+        for (int i=0; i<E; i++){
+            // Add an edge
+            int v = in.readInt(); // read a vertex
+            int w = in.readInt(); // read another vertex
+            addEdge(v, w);        // add edge connecting them
+        }
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public void addEdge(int v, int w){
+        adj[v].add(w);        // add w to v's list
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v){
+        return adj[v];
+    }
+
+    public String toString(){
+        String s = V + " vertices, " + E + " edges\n";
+        for (int v=0; v < V; v++){
+            s += v + ": ";
+            for (int w : this.adj[v])
+                s += w + " ";
+            s += "\n";
+        }
+        return s;
+    }
+
+    public Digraph reverse(){
+        Digraph R = new Digraph(V);
+        for (int v=0; v<V; v++)
+            for (int w: adj(v))
+                R.addEdge(w, v);
+            return R;
+    }
+
+    public static void main(String[] args){
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        StdOut.println(G);
+    }
+}
---
+++ src/DirectedDFS.java
@@ -0,0 +1,47 @@
+/*
+ * Sample usage: java-alg4 Graph tinyDG.txt 1
+ * Sample usage: java-alg4 Graph tinyDG.txt 2
+ * Sample usage: java-alg4 Graph tinyDG.txt 1 2 6
+ */
+
+public class DirectedDFS {
+    private boolean[] marked;   // marked[v] = is there a s-v path?
+
+    public DirectedDFS(Digraph G, int s){
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    public DirectedDFS(Digraph G, Iterable<Integer> sources){
+        marked = new boolean[G.V()];
+        for (int s: sources)
+            if (!marked[s])
+                dfs(G, s);
+    }
+
+    private void dfs(Digraph G, int v){
+        marked[v] = true;
+        for (int w : G.adj(v))
+            if (!marked[w])
+                dfs(G, w);
+    }
+
+    public boolean marked(int w){
+        return marked[w];
+    }
+
+    public static void main(String[] args){
+        Digraph G = new Digraph(new In(args[0]));
+
+        LinkedBag<Integer> sources = new LinkedBag<>();
+        for (int i=1; i<args.length; i++)
+            sources.add(Integer.parseInt(args[i]));
+
+        DirectedDFS reachable = new DirectedDFS(G, sources);
+
+        for (int v=0; v<G.V(); v++)
+            if (reachable.marked(v))
+                StdOut.println(v + " ");
+        StdOut.println();
+    }
+}
