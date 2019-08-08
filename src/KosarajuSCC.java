55179779d2ae266a2bfd2f9fedd6a5760f52e42f
Shiji Zhao <zhaoshiji2016@gmail.com>
2019-08-07 18:37:58 -0700

    Finish section 4.2

---
+++ src/KosarajuSCC.java
@@ -0,0 +1,66 @@
+/*
+ * Sample usage: java-alg4 KosarajuSCC tinyDG.txt
+ * Sample usage: java-alg4 KosarajuSCC mediumDG.txt
+ */
+
+public class KosarajuSCC {
+    private boolean[] marked;
+    private int[] id;           // id[v] = id of connected component containing v
+    private int count;          // number of strong components
+
+    public KosarajuSCC(Digraph G){
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        DepthFirstOrder order = new DepthFirstOrder(G.reverse());
+        for (int s: order.reversePost())
+            if (!marked(s)){
+                dfs(G, s);
+                count++;
+            }
+    }
+
+    private void dfs(Digraph G, int v){
+        marked[v] = true;
+        id[v] = count;
+        for (int w : G.adj(v))
+            if (!marked[w])
+                dfs(G, w);
+    }
+
+    public boolean marked(int w){
+        return marked[w];
+    }
+
+    public int count(){
+        return count;
+    }
+
+    public boolean stronglyConnected(int w, int v){
+        return  id[w]==id[v];
+    }
+
+    public int id(int v){
+        return id[v];
+    }
+
+    public static void main(String[] args){
+        Digraph G = new Digraph(new In(args[0]));
+        KosarajuSCC cc = new KosarajuSCC(G);
+
+        int M = cc.count();
+        StdOut.println(M + " components");
+
+        LinkedBag<Integer>[] components;
+        components = (LinkedBag[]) new LinkedBag[M];
+
+        for (int i=0; i<M; i++)
+            components[i] = new LinkedBag<Integer>();
+        for (int v=0; v<G.V(); v++)
+            components[cc.id(v)].add(v);
+        for (int i=0; i<M; i++){
+            for (int v: components[i])
+                StdOut.print(v + " ");
+            StdOut.println();
+        }
+    }
+}
---
+++ src/TransitiveClosure.java
@@ -0,0 +1,41 @@
+/*
+ * Sample usage: java-alg4 TransitiveClosure tinyDG.txt
+ * Sample usage: java-alg4 TransitiveClosure mediumDG.txt
+ */
+
+public class TransitiveClosure {
+    private DirectedDFS[] all;
+
+    public TransitiveClosure(Digraph G){
+        all = new DirectedDFS[G.V()];
+        for (int v=0; v<G.V(); v++)
+            all[v] = new DirectedDFS(G, v);
+    }
+
+    public boolean reachable(int v, int w){
+        return all[v].marked(w);
+    }
+
+    public static void main(String[] args){
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+
+        TransitiveClosure tc = new TransitiveClosure(G);
+
+        StdOut.print("     ");
+        for (int v = 0; v < G.V(); v++)
+            StdOut.printf("%3d", v);
+        StdOut.println();
+        StdOut.println("--------------------------------------------");
+
+        // print transitive closure
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.printf("%3d: ", v);
+            for (int w = 0; w < G.V(); w++) {
+                if (tc.reachable(v, w)) StdOut.print("  T");
+                else                    StdOut.print("   ");
+            }
+            StdOut.println();
+        }
+    }
+}
